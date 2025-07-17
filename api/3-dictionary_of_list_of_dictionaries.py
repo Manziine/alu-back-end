7c2afc47e91db4a17f6ee110b65dde516ab127b5
Manzi Ineza <a.manziine@alustudent.com>
2025-07-17 10:56:04 -0700

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,52 +1,23 @@
 #!/usr/bin/python3
-"""
-Exports all employees' TODO tasks to a JSON file.
-"""
+"""export Json"""
+
 
 import json
 import requests
 
 
-def export_all_tasks_to_json():
-    """Export all employees' tasks to a JSON file."""
-    # Base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Get all users
-    users_response = requests.get(f"{base_url}/users")
-    users = users_response.json()
-
-    # Get all todos
-    todos_response = requests.get(f"{base_url}/todos")
-    todos = todos_response.json()
-
-    # Create a dictionary to hold all tasks organized by user ID
-    all_tasks = {}
-
-    for user in users:
-        user_id = user["id"]
-        username = user["username"]
-        
-        # Filter todos for this user
-        user_todos = [todo for todo in todos if todo["userId"] == user_id]
-        
-        # Format the tasks for this user
-        tasks_list = []
-        for todo in user_todos:
-            task_dict = {
-                "username": username,
-                "task": todo["title"],
-                "completed": todo["completed"]
-            }
-            tasks_list.append(task_dict)
-        
-        # Add to the main dictionary
-        all_tasks[str(user_id)] = tasks_list
-
-    # Write to JSON file
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(all_tasks, jsonfile, indent=4)
-
-
 if __name__ == "__main__":
-    export_all_tasks_to_json() 
+    def getTodos(id):
+        link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        res = requests.get(link)
+        return json.loads(res.text)
+    link = "https://jsonplaceholder.typicode.com/users/"
+    res = requests.get(link)
+    users = json.loads(res.text)
+    data = {}
+    for i in users:
+        todos = getTodos(i["id"])
+        data[i["id"]] = [{"task": j["title"], "completed": j["completed"],
+                          "username": i["username"]} for j in todos]
+    with open("todo_all_employees.json", 'w', encoding='utf-8') as f:
+        f.write(json.dumps(data))
